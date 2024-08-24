15d63604450d0212d66f7c8591408b998ba7574b
EnDiGi <degiorgienrico.26042012@gmail.com>
2024-08-24 17:50:21 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,59 @@
+const baseUrl = "https://pokeapi.co/api/v2/";
+
+searchBar = document.getElementById("searchBar");
+searchBtn = document.getElementById("searchBtn");
+urlInfo = document.getElementById("urlInfo");
+
+pokemonSprite = document.getElementById("pokemonSprite");
+pokemonName = document.getElementById("pokemonName");
+pokemonType = document.getElementById("pokemonType");
+pokemonId = document.getElementById("pokemonId");
+pokemonWeight = document.getElementById("pokemonWeight");
+pokemonHeight = document.getElementById("pokemonHeight");
+
+elementsToShow = Array.from(document.getElementsByClassName("toShow"))
+
+function capitalize(element){return element[0].toUpperCase() + element.slice(1)}
+
+function displayData(data, url){
+    urlInfo.textContent = "Url: " + url;
+    console.log(data);
+
+    pokemonType.textContent = "";
+
+    elementsToShow.forEach(function(element){element.style.visibility = "visible"});
+
+    pokemonName.textContent = capitalize(data.name.replace("-", " "));
+    pokemonSprite.src = data.sprites.front_default;
+    
+    types = Array.from(data.types);
+    types.forEach(function(element){pokemonType.textContent += capitalize(element.type.name) + "\n"});
+
+    pokemonId.textContent = data.id;
+    pokemonWeight.textContent = data.weight
+    pokemonHeight.textContent = data.height
+}
+
+function displayError(error){
+    urlInfo.textContent = "Pokemon not found";
+    console.log(error)
+}
+
+function searchPokemon(name){
+    const url = baseUrl + "pokemon/" + name.toLowerCase().replace(" ", "-");
+
+    fetch(url)
+         .then(response => response.json())
+         .then(data => displayData(data, url))
+         .catch(error => {displayError(error)})
+}
+
+searchBtn.onclick = function(){searchPokemon(searchBar.value)}
+
+document.addEventListener("keydown", function(event){
+    if(event.key === "Enter"){
+        searchPokemon(searchBar.value);
+    }
+})
+
+searchPokemon("charmander")
